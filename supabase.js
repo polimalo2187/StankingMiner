f8942e4a1a0ad85a125be43d84320103fbda25f4
polimalo2187 <carloramirez834@gmail.com>
2025-12-02 09:24:37 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 dotenv.config();
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE
+  process.env.SUPABASE_KEY
 );

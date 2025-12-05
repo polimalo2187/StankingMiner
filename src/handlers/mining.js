7afc526e2e6e6e30bd2186e3be1eab6a2d0646a3
polimalo2187 <carloramirez834@gmail.com>
2025-12-05 01:01:53 -0500

    Update mining.js

--- src/handlers/mining.js
+++ src/handlers/mining.js
@@ -1,5 +1,5 @@
 import { Markup } from "telegraf";
-import { supabase } from "../../supabase.js";
+import { supabase } from "../supabase.js";
 
 const MIN_VALID_REFERRALS = 5;
 const MINING_REWARD = 0.02;

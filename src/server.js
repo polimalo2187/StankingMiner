85b4533a5a36b774a04e14b22650b9f069e98713
polimalo2187 <carloramirez834@gmail.com>
2025-12-09 15:29:28 -0500

    Delete src/server.js

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import http from "http";
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Bot running\n");
-  })
-  .listen(process.env.PORT || 3000);
